8260b005c5f24cfc51c58f112bca63d260e86130
IlyaBal <ilya.bal.2011@gmail.com>
2023-07-18 11:18:01 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './index.css';
 
-const router = createBrowserRouter([
-  {
-    path: '/app3',
-    element: <div>Hello world!</div>,
-  },
-  { path: '/app3/app', element: <App /> },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '/app3',
+      element: <div>Hello world!</div>,
+    },
+    { path: '/app', element: <App /> },
+  ],
+  { basename: '/app3' }
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
