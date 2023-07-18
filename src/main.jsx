d190ddb46e624db619d94a35a1188e323b0ef64f
IlyaBal <ilya.bal.2011@gmail.com>
2023-07-18 11:10:59 +0200

    routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/app3',
+    element: <div>Hello world!</div>,
+  },
+  { path: '/app3/app', element: <App /> },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
